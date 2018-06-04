b0f318985af640a9a3dab6878ec7de15de50fec2
Evgeny <seedofpanic@mail.ru>
2018-06-04 15:10:48 +0300

    auto open safe fields

--- src/store/actions/openField.js
+++ src/store/actions/openField.js
@@ -1,11 +1,33 @@
 import {STORE} from '../index';
 
 export function actionOpenField(x, y) {
+    let map = STORE.getState().map;
+
+    if (map[x][y].show) {
+        return;
+    }
+
     STORE.dispatch({
         type: 'openField',
         payload: {
             x,
             y
         }
-    })
-};
+    });
+
+    map = STORE.getState().map;
+
+    if (map[x][y].danger !== 0) {
+        return;
+    }
+
+    for (let i = -1; i < 2; i++) {
+        for (let j = -1; j < 2; j++) {
+            if (map[x + i] && map[x + i][y + j] && !map[x + i][y + j].show) {
+                setTimeout(() => {
+                    actionOpenField(x + i, y + j);
+                }, 100);
+            }
+        }
+    }
+}

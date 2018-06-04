b1792ddbb4ee7a56cfd1ad6aefa177219cef2d25
Evgeny <seedofpanic@mail.ru>
2018-06-04 21:06:37 +0300

    hot fix

--- src/store/index.js
+++ src/store/index.js
@@ -52,9 +52,9 @@ export const Reducers = {
             ...state.map
         ];
 
-        map[payload.x] = {
+        map[payload.x] = [
             ...map[payload.x]
-        };
+        ];
 
         map[payload.x][payload.y] = {
             ...map[payload.x][payload.y],
